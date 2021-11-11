97880a5cc807dad304d31cdf7c0accbac497b514
Alexander Nelovkin <72705638+Nikvolen@users.noreply.github.com>
2021-11-11 21:50:40 +0300

    Update Advert.py
    Программа доработана, в соответствии с замечаниями преподавателя.

--- Advert.py
+++ Advert.py
@@ -8,55 +8,59 @@ class AdvertAttributes:
         for key, value in self.attributes.items():
             if keyword.iskeyword(key):
                 key = f'{key}_'
+            if key == 'price':
+                self.price = value
             if isinstance(value, dict):
-                self.__setattr__(key, AdvertAttributes(value))
-            else:
-                self.__setattr__(key, value)
-
-        if not self.attributes.get('price', 0):
-            self.__dict__['price'] = 0
-        elif self.attributes['price'] < 0:
-            raise ValueError('Price must be >= 0')
-
-    def __setattr__(self, key, value):
-        self.__dict__[key] = value
-
-
-class JsonData:
-    def __init__(self, raw_data):
-        self.raw_data = raw_data
-
-    def get_python_object(self) -> dict:
-        return json.loads(self.raw_data)
+                value = AdvertAttributes(value)
+            self.__dict__[key] = value
 
 
 class ColorizeMixin:
     def __repr__(self):
-        return f'\033[0;{self.repr_color_code}m{self.title} | {self.price} ₽\033[0;0m'
+        text = super().__repr__()
+        return f'\033[0;{self.repr_color_code}m{text} \033[0;0m'
 
 
-class Advert(ColorizeMixin, AdvertAttributes):
+class BaseAdvert:
+    def __repr__(self):
+        return f'{self.title} || {self.price} ₽'
+
+
+class Advert(ColorizeMixin, AdvertAttributes, BaseAdvert):
     repr_color_code = 33  # Yellow
 
     def __init__(self, attributes):
         super().__init__(attributes)
+        self.price
+        if not hasattr(self, 'title'):
+            raise ValueError('Tittle not setted')
 
-    def __repr__(self):
-        return f'\033[0;{self.repr_color_code}m{self.title} | {self.price} ₽\033[0;0m'
+
+    @property
+    def price(self):
+        if not hasattr(self, 'price_'):
+            setattr(self, 'price_', 0)
+        return self.price_
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('Price must be int or float')
+        elif value < 0:
+            raise ValueError('Price must be >= 0')
+        self.price_ = value
 
 
 if __name__ == '__main__':
     ex1 = """{ 
         "title": "Drag X", 
-        "price": 3490, 
         "location": { 
             "address": "город Москва, Лесная, 7", 
             "metro_stations": ["Белорусская"] 
             } 
         }"""
 
-    ex2 = """{ 
-        "title": "python",  
+    ex2 = """{   
         "location": { 
             "address": "город Москва, Лесная, 7", 
             "metro_stations": ["Белорусская"] 
@@ -72,9 +76,10 @@ if __name__ == '__main__':
             } 
         }"""
 
-    ad1 = Advert(JsonData(ex3).get_python_object())
+    ad1 = Advert(json.loads(ex3))
     print(ad1.title)
     print(ad1.price)
     print(ad1.class_)
     print(ad1.location.address)
     print(ad1)
+
